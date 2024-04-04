fd69b83bf0756dc5efc8701f6802b621d9aab201
Rodney Aguinaldo <rodney.aguinal@gmail.com>
2024-04-05 05:01:21 +0800

    added initial commit

---
+++ src/main/java/com/democompany/first_demo_project2/App.java
@@ -0,0 +1,13 @@
+package com.democompany.first_demo_project2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
